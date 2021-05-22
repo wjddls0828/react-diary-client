6a3028ab4678eab496fe8c71df199d118c3fe8e1
dewyeon <juyon.ko@gmail.com>
2021-05-22 23:53:08 +0900

    feat: 글 작성 페이지 레이아웃

---
+++ views/post-input/index.tsx
@@ -0,0 +1,24 @@
+import Layout from 'components/layout/index';
+import { Button } from 'primereact/button';
+import * as S from './styles';
+
+const PostInputPage = () => {
+  return (
+    <Layout>
+      <S.PostInputPageContainer>
+        <S.EditorContainer> 
+          <textarea id="post-content" cols={80} rows={30}></textarea>
+        </S.EditorContainer>
+        <S.TagComponentContainer>
+          (태그 컴포넌트 위치)
+        </S.TagComponentContainer>
+        <S.ButtonContainer>
+            <Button>임시저장</Button>
+            <Button>업로드</Button>
+        </S.ButtonContainer>
+      </S.PostInputPageContainer>
+    </Layout>
+  );
+};
+
+export default PostInputPage;
