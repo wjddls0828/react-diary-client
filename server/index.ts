2216871ac5daab81a67438f38efced64473bf93b
swimme <2sy100@likelion.org>
2021-05-25 23:52:02 +0900

    chore: cookie-parser 패키지 설치 for express

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import postRouter from './routes/post';
 import authRouter from './routes/auth';
+import cookieParser from 'cookie-parser';
 
 const express = require('express');
 const next = require('next');
@@ -15,6 +16,8 @@ app
   .prepare()
   .then(() => {
     const server = express();
+    server.use(cookieParser());
+    server.use(express.json());
 
     server.use('/api/auth', authRouter);
     server.use('/api/posts', postRouter);
