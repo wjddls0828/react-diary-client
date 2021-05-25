8aa618a8338084ba76018906c6d6e1558abebd12
swimme <2sy100@likelion.org>
2021-05-25 23:48:55 +0900

    refactor: try-catch -> promise chaining 코드스타일 변경

--- server/service/userService.ts
+++ server/service/userService.ts
@@ -6,59 +6,59 @@ import * as jwt from 'jsonwebtoken';
 import { parseRawData } from '../utils/parseRawData';
 export class UserModel {
   static async findUserByEmail(email: string): Promise<User[]> {
-    const connection: PoolConnection = await DBPool.getConnection();
-    try {
-      const [user] = await connection.query(`SELECT * FROM user WHERE email = ? LIMIT 1`, [email]);
+    const [user] = await DBPool.query(`SELECT * FROM user WHERE email = ? LIMIT 1`, [email]).catch(
+      async (err) => {
+        console.log(err.message);
+        throw new Error();
+      }
+    );
 
-      return parseRawData(user);
-    } catch (err) {
-      console.error(err.message);
-      throw new Error();
-    } finally {
-      connection.release();
-    }
+    return parseRawData(user);
   }
 
   static async createUser(userInfo: GoogleUserDTO): Promise<User[]> {
     const connection: PoolConnection = await DBPool.getConnection();
-    try {
-      await connection.beginTransaction();
+    await connection.beginTransaction();
 
-      const defaultBlogTitle = `${userInfo.name}'s Diary`;
-      await connection.query(`INSERT INTO user VALUES (null,?,?,?)`, [
+    const defaultBlogTitle = `${userInfo.name}'s Diary`;
+
+    return await connection
+      .query(`INSERT INTO user VALUES (null,?,?,?)`, [
         userInfo.email,
         userInfo.name,
         defaultBlogTitle,
-      ]);
+      ])
+      .then(async () => {
+        const newUser = await this.findUserByEmail(userInfo.email);
+        await connection.commit();
 
-      const newUser = await this.findUserByEmail(userInfo.email);
-      await connection.commit();
+        return parseRawData(newUser);
+      })
+      .catch(async (err) => {
+        await connection.rollback();
 
-      return parseRawData(newUser);
-    } catch (err) {
-      await connection.rollback();
-
-      console.error(err.message);
-      throw new Error();
-    } finally {
-      connection.release();
-    }
+        console.error(err.message);
+        throw new Error();
+      })
+      .finally(() => {
+        connection.release();
+      });
   }
 
   public static async validateUser(userInfo: GoogleUserDTO): Promise<User> {
-    try {
-      const user = await this.findUserByEmail(userInfo.email);
+    return await this.findUserByEmail(userInfo.email)
+      .then(async (user) => {
+        if (!user.length) {
+          const newUser = await this.createUser(userInfo);
+          return newUser[0];
+        }
 
-      if (!user.length) {
-        const newUser = await this.createUser(userInfo);
-        return newUser[0];
-      }
-
-      return user[0];
-    } catch (err) {
-      console.error(err.message);
-      throw new Error();
-    }
+        return user[0];
+      })
+      .catch((err) => {
+        console.error(err.message);
+        throw new Error();
+      });
   }
 
   public static createAccessToken(user: User): string {
