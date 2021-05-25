e6941265cfccf01ce296d2215b9718e3557d9234
swimme <2sy100@likelion.org>
2021-05-25 23:58:05 +0900

    fix: 게시글 조회 api -> 유저가 쓴 게시글만 조회하도록 수정

--- server/service/postService.ts
+++ server/service/postService.ts
@@ -1,19 +1,16 @@
 import DBPool from './config';
-import { Post } from '../../share/interfaces/post';
-import { PoolConnection } from 'mysql2/promise';
+import { Post, PostRequestBody } from '../../share/interfaces/post';
 import { parseRawData } from '../utils/parseRawData';
 
 export default class PostService {
-  public static async getAllPosts(): Promise<Post[]> {
-    try {
-      const connection: PoolConnection = await DBPool.getConnection();
-      const [data] = await connection.query(`select * from post`);
-      connection.release();
+  public static async getAllPosts(userId: number): Promise<Post[]> {
+    const [data] = await DBPool.query(`select * from post where userId = ?`, [userId]).catch(
+      (err) => {
+        console.error(err.message);
+        throw new Error();
+      }
+    );
 
-      return parseRawData(data);
-    } catch (err) {
-      console.error(err);
-      throw new Error();
-    }
+    return parseRawData(data);
   }
 }
