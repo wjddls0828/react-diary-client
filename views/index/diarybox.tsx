3d2e2eed05ff8659cc1daacbf439d28bf08890a5
jeongin yun <dhrehd1234@naver.com>
2021-06-02 18:20:25 +0900

    title영역을 createdAt의 날짜 표시로 변경했습니다. 추후에 이모티콘 추가하면서 말씀해주신 변수와 컴포넌트 구성 변경할 예정입니다

--- views/index/diarybox.tsx
+++ views/index/diarybox.tsx
@@ -3,52 +3,41 @@ import { Panel } from 'primereact/panel';
 import { Ripple } from 'primereact/ripple';
 import * as S from './styles';
 
-const Diarywriting = ({post}) => {
-    const {id, userId,title,content}=post;
-    return (
-        <S.One_box>
-        <Panel header={title}>
-            <p>{content}</p>
-        </Panel>
-        </S.One_box>
-    )
-}
+const Diarywriting = ({ post }) => {
+  const { id, userId, createdAt, content } = post;
+  return (
+    <S.One_box>
+      <Panel header={createdAt}>
+        <p>{content}</p>
+      </Panel>
+    </S.One_box>
+  );
+};
 
+const Diarybox = ({ posts }) => {
+  console.log({ posts });
 
-
-
-const Diarybox = ({posts}) => {
-    console.log({posts})
-    
-
-    const template = (options) => {
-        const toggleIcon = options.collapsed ? 'pi pi-chevron-down' : 'pi pi-chevron-up';
-        const className = `${options.className} p-jc-start`;
-        const titleClassName = `${options.titleClassName} p-pl-1`;
-
-        return (
-            <div className={className}>
-                <button className={options.togglerClassName} onClick={options.onTogglerClick}>
-                    <span className={toggleIcon}></span>
-                    <Ripple />
-                </button>
-                <span className={titleClassName}>
-                    Header
-                </span>
-            </div>
-        )
-    }
+  const template = (options) => {
+    const toggleIcon = options.collapsed ? 'pi pi-chevron-down' : 'pi pi-chevron-up';
+    const className = `${options.className} p-jc-start`;
+    const titleClassName = `${options.titleClassName} p-pl-1`;
 
     return (
-        <div>
-            <Diarywriting post={posts}/>
-        </div>
-    )
-}
+      <div className={className}>
+        <button className={options.togglerClassName} onClick={options.onTogglerClick}>
+          <span className={toggleIcon}></span>
+          <Ripple />
+        </button>
+        <span className={titleClassName}>Header</span>
+      </div>
+    );
+  };
 
-
-
-
-
+  return (
+    <div>
+      <Diarywriting post={posts} />
+    </div>
+  );
+};
 
 export default Diarybox;
