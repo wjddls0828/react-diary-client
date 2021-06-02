632378ba8f44c00d514c1483c760914b13accf08
jeongin yun <dhrehd1234@naver.com>
2021-06-02 18:28:02 +0900

    chore: yarn add react-paginate 패키지 설치, 레퍼런스(https://www.youtube.com/watch?v=HANSMtDy508)

--- views/index/index.tsx
+++ views/index/index.tsx
@@ -6,14 +6,12 @@ import * as S from './styles';
 import Sidebar from 'views/components/sidebar';
 import Search from './search';
 import Diarybox from './diarybox';
-import React, {useState} from 'react';
-import ReactPaginate from "react-paginate";
-
+import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
 interface IndexPageProps {
   posts: Post[];
 }
-
 
 const IndexPage: NextPage<IndexPageProps> = ({ posts }) => {
   console.log(posts);
@@ -23,21 +21,17 @@ const IndexPage: NextPage<IndexPageProps> = ({ posts }) => {
 
   const usersPerPage = 10;
   const pagesVisited = pageNumber * usersPerPage;
-  
-  const diarybox = users
-    .slice(pagesVisited, pagesVisited + usersPerPage)
-    .map((posts) => {
-      return (
-        <Diarybox posts={posts} />
-      );
-    });
+
+  const diarybox = users.slice(pagesVisited, pagesVisited + usersPerPage).map((posts) => {
+    return <Diarybox posts={posts} />;
+  });
 
   const pageCount = Math.ceil(users.length / usersPerPage);
 
   const changePage = ({ selected }) => {
     setPageNumber(selected);
   };
-  
+
   return (
     <Layout>
       <Sidebar />
@@ -45,20 +39,17 @@ const IndexPage: NextPage<IndexPageProps> = ({ posts }) => {
         <Search />
         {diarybox}
         <ReactPaginate
-        previousLabel={"이전"}
-        nextLabel={"다음"}
-        pageCount={pageCount}
-        onPageChange={changePage}
-        containerClassName={"pagebtn"}
-        activeClassName={"page_active_btn"}
-      />
+          previousLabel={'이전'}
+          nextLabel={'다음'}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={'pagebtn'}
+          activeClassName={'page_active_btn'}
+        />
       </S.Mainpage>
-      
     </Layout>
   );
 };
-
-
 
 export default IndexPage;
 
@@ -69,5 +60,3 @@ export async function getServerSideProps() {
     props: { posts: data },
   };
 }
-
-
