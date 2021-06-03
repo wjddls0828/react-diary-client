0bd2f94af74823d6d99bd84b1c6445d83f6f591d
dewyeon <juyon.ko@gmail.com>
2021-06-03 17:30:35 +0900

    chore: PostRequestBody 임시 삭제 (에러 방지)

--- server/service/postService.ts
+++ server/service/postService.ts
@@ -1,5 +1,5 @@
 import DBPool from './config';
-import { Post, PostRequestBody } from '../../share/interfaces/post';
+import { Post } from '../../share/interfaces/post';
 import { parseRawData } from '../utils/parseRawData';
 
 export default class PostService {
