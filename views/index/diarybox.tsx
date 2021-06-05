47433a992e5a50e28b152e4fb8f2eb602a6ec327
swimme <2sy100@likelion.org>
2021-06-05 17:02:40 +0900

    MERGE dev into feature/editor

---
+++ views/index/diarybox.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Panel } from 'primereact/panel';
+import { Ripple } from 'primereact/ripple';
+import * as S from './styles';
+
+const Diarywriting = ({ post }) => {
+  const { id, userId, createdAt, content } = post;
+  return (
+    <S.One_box>
+      <Panel header={createdAt}>
+        <p>{content}</p>
+      </Panel>
+    </S.One_box>
+  );
+};
+
+const Diarybox = ({ posts }) => {
+  console.log({ posts });
+
+  const template = (options) => {
+    const toggleIcon = options.collapsed ? 'pi pi-chevron-down' : 'pi pi-chevron-up';
+    const className = `${options.className} p-jc-start`;
+    const titleClassName = `${options.titleClassName} p-pl-1`;
+
+    return (
+      <div className={className}>
+        <button className={options.togglerClassName} onClick={options.onTogglerClick}>
+          <span className={toggleIcon}></span>
+          <Ripple />
+        </button>
+        <span className={titleClassName}>Header</span>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Diarywriting post={posts} />
+    </div>
+  );
+};
+
+export default Diarybox;
--- views/index/index.tsx
+++ views/index/index.tsx
@@ -3,7 +3,11 @@ import { NextPage } from 'next';
 import { Post } from 'share/interfaces/post';
 import * as S from './styles';
 import Sidebar from 'views/components/sidebar';
-import postAPI from 'common/api/postAPI';
+import Search from './search';
+import Diarybox from './diarybox';
+import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import { getMockdata } from 'share/utils/mock-data';
 
 interface IndexPageProps {
   posts: Post[];
@@ -12,10 +16,37 @@ interface IndexPageProps {
 const IndexPage: NextPage<IndexPageProps> = ({ posts }) => {
   console.log(posts);
 
+  const [users, setUsers] = useState(posts.slice(0, 50));
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const usersPerPage = 10;
+  const pagesVisited = pageNumber * usersPerPage;
+
+  const diarybox = users.slice(pagesVisited, pagesVisited + usersPerPage).map((posts) => {
+    return <Diarybox posts={posts} />;
+  });
+
+  const pageCount = Math.ceil(users.length / usersPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   return (
     <Layout>
       <Sidebar />
-      <S.IndexPage>메인 페이지</S.IndexPage>
+      <S.Mainpage>
+        <Search />
+        {diarybox}
+        <ReactPaginate
+          previousLabel={'이전'}
+          nextLabel={'다음'}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={'pagebtn'}
+          activeClassName={'page_active_btn'}
+        />
+      </S.Mainpage>
     </Layout>
   );
 };
@@ -23,7 +54,7 @@ const IndexPage: NextPage<IndexPageProps> = ({ posts }) => {
 export default IndexPage;
 
 export async function getServerSideProps() {
-  const posts = await postAPI.getPostById(1);
+  const posts = await getMockdata();
 
   if (!posts) {
     return { props: { posts: null } };
