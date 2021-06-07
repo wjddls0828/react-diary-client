7fb3eb0267765b3dc049ba40b57a7d311fe7fe6b
swimme <2sy100@likelion.org>
2021-06-07 23:23:03 +0900

    feat: SSR로 기분별 게시글개수 받아오기

--- views/index/index.tsx
+++ views/index/index.tsx
@@ -1,38 +1,44 @@
 import Layout from 'views/components/layout/index';
 import { NextPage } from 'next';
-import { PagedPosts, Post } from 'share/interfaces/post';
 import * as S from './styles';
 import Sidebar from 'views/components/sidebar';
-import Search from './search';
 import Diarybox from './diarybox';
-import React from 'react';
+import React, { useMemo } from 'react';
 import ReactPaginate from 'react-paginate';
 import postAPI from 'common/api/postAPI';
 import { usePagedPosts } from './hooks';
 import Emptybox from './emptybox';
+import { PagedPosts, Post, PostCountsByMoodId } from 'share/interfaces/post';
+import { getCurrentYearMonth } from 'common/utils/getCurrentYearMonth';
 interface IndexPageProps {
   initialPosts: Post[];
   total: number;
+  moodCounts: PostCountsByMoodId[];
 }
 
-const IndexPage: NextPage<IndexPageProps> = ({ initialPosts, total }) => {
+const IndexPage: NextPage<IndexPageProps> = ({ initialPosts, total, moodCounts }) => {
   const { pageCount, changePage, pagedPosts } = usePagedPosts({ initialPosts, total });
 
   return (
     <Layout>
       <Sidebar />
-      <S.Mainpage>
-        <Search />
 
-        <S.DiaryContainer>
-          {pagedPosts.length ? (
-            pagedPosts.map((post) => {
-              return <Diarybox key={post.id} post={post} />;
-            })
-          ) : (
-            <Emptybox />
-          )}
-        </S.DiaryContainer>
+      <S.Mainpage>
+        <S.MonthlyMoodCountContainer>
+          <S.MoodCountContainerTitle>이번 달 내 기분은 ...</S.MoodCountContainerTitle>
+          {moodCounts &&
+            moodCounts.map((moodCount: PostCountsByMoodId) => {
+              return <div key={moodCount.moodId}>{moodCount.count}</div>;
+            })}
+        </S.MonthlyMoodCountContainer>
+
+        {pagedPosts.length ? (
+          pagedPosts.map((post) => {
+            return <Diarybox key={post.id} post={post} />;
+          })
+        ) : (
+          <Emptybox />
+        )}
 
         {pageCount > 0 && (
           <ReactPaginate
@@ -55,13 +61,16 @@ export default IndexPage;
 
 export async function getServerSideProps() {
   const data: PagedPosts = await postAPI.getAllPostsByPage(1);
-  const { total, posts } = data;
+  const term = getCurrentYearMonth(); // TODO - SSR ok?
+  const moodCounts = await postAPI.getMonthlyMoodPostCounts(term);
 
-  if (!data) {
-    return { props: { total: 0, initialPosts: [] } };
+  if (!data || !moodCounts) {
+    return { props: { total: 0, initialPosts: [], moodCounts: [] } };
   }
 
+  const { total, posts } = data;
+
   return {
-    props: { total, initialPosts: posts },
+    props: { total, initialPosts: posts, moodCounts },
   };
 }
--- views/index/styles.ts
+++ views/index/styles.ts
@@ -1,21 +1,31 @@
+import { THEME_COLOR } from 'common/constant';
 import styled from 'styled-components';
 
 export const Mainpage = styled.div`
   width: 100%;
-  max-height: 800px;
-  background-color: white;
+  max-height: 1000px;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-export const DiaryContainer = styled.div`
+export const MonthlyMoodCountContainer = styled.div`
   width: 100%;
 
   display: flex;
   flex-direction: column;
-  align-items: center;
+  padding: 30px;
+
+  background-color: white;
   margin-bottom: 30px;
+`;
+
+export const MoodCountContainerTitle = styled.div`
+  font-size: 20px;
+  font-weight: 700;
+  margin-bottom: 10px;
+
+  color: ${THEME_COLOR.BROWN};
 `;
 
 export const Search_box = styled.div`
