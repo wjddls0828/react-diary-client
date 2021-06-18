25d6ca2a068064d04164b5aeb3951bbcc95e8722
swimme <2sy100@likelion.org>
2021-06-18 16:46:54 +0900

    feat: 캘린더에 월별 게시글이벤트추가+ 월별기분통계 연동

--- views/index/styles.ts
+++ views/index/styles.ts
@@ -1,43 +1,24 @@
-import { THEME_COLOR } from 'common/constant';
 import styled from 'styled-components';
 
 export const Mainpage = styled.div`
   width: 100%;
-  max-height: 1000px;
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
 
-export const MonthlyMoodCountContainer = styled.div`
-  width: 100%;
-
-  display: flex;
-  flex-direction: column;
-  padding: 30px;
-
-  background-color: white;
-  margin-bottom: 30px;
-`;
-
-export const MoodCountContainerTitle = styled.div`
-  font-size: 20px;
-  font-weight: 700;
-  margin-bottom: 10px;
-
-  color: ${THEME_COLOR.BROWN};
+  padding-bottom: 30px;
 `;
 
 export const DiaryListContainer = styled.div`
   width: 100%;
-
+  height: 100% auto;
   background-color: white;
-
   padding: 30px;
 
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-bottom: 30px;
 `;
 
 export const DiaryBoxContainer = styled.div`
@@ -54,4 +35,12 @@ export const DiaryBoxContainer = styled.div`
 
 export const SearchBox = styled.div`
   margin: 30px;
+`;
+
+export const CalendarContainer = styled.div`
+  width: 100%;
+  height: 100% auto;
+
+  background-color: white;
+  padding: 30px;
 `;
