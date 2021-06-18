a3dc962a88c56f66c5de5790ee7893883526fcbf
swimme <2sy100@likelion.org>
2021-06-18 20:43:01 +0900

    refactor: [css] 헤더 폰트 크기, 두께 조정 / 사이드바 너비 줄임

--- views/components/sidebar/styles.ts
+++ views/components/sidebar/styles.ts
@@ -1,10 +1,13 @@
+import { THEME_COLOR } from 'common/constant';
 import styled from 'styled-components';
 
 export const Sidebar = styled.div`
   display: flex;
   flex-direction: column;
 
-  width: 300px;
+  width: 200px;
+  height: 1000px;
+
   padding: 15px;
   margin-right: 15px;
   margin-bottom: 15px;
@@ -15,9 +18,29 @@ export const Sidebar = styled.div`
 
 export const UserProfile = styled.div`
   width: 100%;
+  height: 30px;
+
   margin-bottom: 10px;
 
   color: #7b6d6c;
   font-weight: bolder;
   font-size: 15px;
+
+  display: flex;
+  align-items: center;
+`;
+
+export const LogoutButton = styled.button`
+  border: 1px solid #c1b7b7;
+  background-color: transparent;
+  width: 50px;
+  float: right;
+  height: 20px;
+  font-size: 10px;
+  margin-left: auto;
+  :hover {
+    background-color: #c1b7b7;
+  }
+
+  color: ${THEME_COLOR.BROWN};
 `;
