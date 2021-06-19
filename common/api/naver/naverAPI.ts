f41907824b14c1041bad5e5b903f818bbe4d5f86
swimme <2sy100@likelion.org>
2021-06-20 01:21:41 +0900

    refactor: 책검색 api display쿼리 상수에서 파라미터로 변경

--- common/api/naver/naverAPI.ts
+++ common/api/naver/naverAPI.ts
@@ -1,11 +1,14 @@
 import naverAxios from 'common/api/naver/naverAxios';
-import { POSTS_PER_PAGE } from 'share/constant';
 import { PagedNaverBooks } from 'share/interfaces/naverBook';
 
 const naverAPI = {
-  searchBookByKeyword: async (keyword: string, start: number): Promise<PagedNaverBooks> => {
+  searchBookByKeyword: async (
+    keyword: string,
+    perPage: number,
+    start: number
+  ): Promise<PagedNaverBooks> => {
     const pagedBooks = await naverAxios
-      .get(`?query=${keyword}&display=${POSTS_PER_PAGE}&start=${start}`)
+      .get(`?query=${keyword}&display=${perPage}&start=${start}`)
       .catch((err) => {
         console.error(err);
         return null;
