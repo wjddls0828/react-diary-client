80b6a5a5a40a365956d11c35f45a9ddb41f5321e
swimme <2sy100@likelion.org>
2021-06-21 13:37:04 +0900

    hotfix: 불필요한 공백삭제

--- views/index/SearchBar.tsx
+++ views/index/SearchBar.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { Input } from 'antd';
-
 import 'antd/lib/input/style/index.css';
 import 'antd/lib/button/style/index.css';
 
@@ -9,19 +8,15 @@ const { Search } = Input;
 
 const SearchBar: React.FC = () => {
   const router = useRouter();
-
   const { keyword } = router.query;
   const decodedTerm = decodeURIComponent(keyword as string);
-
   const [searchTerm, setSearchTerm] = useState<string>(decodedTerm);
-
   const handleSearchInputChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     setSearchTerm(event.currentTarget.value);
   };
 
   const handleSearchInputSubmit = () => {
     router.push({ pathname: '/search', query: { keyword: encodeURIComponent(searchTerm) } });
-
   };
 
   return (
